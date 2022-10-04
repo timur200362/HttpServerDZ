c032556b375720c31143995379ed409639ea9282
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-10-04 20:51:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using INF2course.Week_4;
+using System.Collections.Generic;
+
+namespace INF2course
+{
+    class ServerClass
+    {
+        private bool running = true;
+        public bool Running { get => running; set { running = value; } }
+        public void Execute()
+        {
+            HttpServer server = new HttpServer(7700, @"./google", "google2.html");
+            server.Start();
+            while (running)
+                Handler(Console.ReadLine()?.ToLower(), server);
+        }
+        delegate void cmd(HttpServer server, ServerClass server_class);
+        static void Stop(HttpServer server, ServerClass server_class) { server.Stop(); }
+        static void Restart(HttpServer server, ServerClass server_class) { server.Start(); server.Stop(); }
+        static void Start(HttpServer server, ServerClass server_class) { server.Start(); }
+        static void Exit(HttpServer server, ServerClass server_class) { server_class.Running = false; }
+
+        Dictionary<string, cmd> dict = new Dictionary<string, cmd> { { "stop", Stop }, { "start", Start }, { "restart", Restart }, { "exit", Exit } };
+        void Handler(string command, HttpServer server)
+        {
+            if (dict.ContainsKey(command))
+                dict[command](server, this);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerClass server_class = new ServerClass();
+            server_class.Execute();
+        }
+
+    }
+}
---
+++ Week_4/HttpServer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2course.Week_4
+{
+    enum ServerStatus { Start, Stop };
+    internal class HttpServer
+    {
+        public ServerStatus Status = ServerStatus.Stop;
+        private readonly HttpListener http_listener;
+        public int Port { get; set; }
+        public string Path { get; set; }
+        public string MainPage { get; set; }
+        public HttpServer(int port, string path, string main_page)
+        {
+            Port = port; Path = path; MainPage = main_page;
+            http_listener = new HttpListener();
+            http_listener.Prefixes.Add($"http://localhost:" + Port + "/");
+        }
+        public void Start()
+        {
+            if (Status == ServerStatus.Start)
+            {
+                Console.WriteLine("Сервер уже запущен");
+                return;
+            }
+            http_listener.Start();
+            Console.WriteLine("Сервер запущен");
+            Status = ServerStatus.Start;
+            Listening();
+        }
+        public void Stop()
+        {
+            if (Status == ServerStatus.Stop)
+            {
+                Console.WriteLine("Сервер уже остановлен");
+                return;
+            }
+            Status = ServerStatus.Stop;
+            http_listener.Stop();
+            Console.WriteLine("Сервер остановлен");
+        }
+        private void Listening()
+        {
+            http_listener.BeginGetContext(new AsyncCallback(ListenerCallBack), http_listener);
+        }
+        private void ListenerCallBack(IAsyncResult result)
+        {
+            if (http_listener.IsListening && Status != ServerStatus.Stop)
+            {
+                var _httpContext = http_listener.EndGetContext(result);
+                HttpListenerRequest request = _httpContext.Request;
+                HttpListenerResponse response = _httpContext.Response;
+                byte[] buffer;
+                if (Directory.Exists(Path))
+                {
+                    buffer = getFile(_httpContext.Request.RawUrl, MainPage);
+                    //Console.ReadKey();
+                }
+                else
+                {
+                    string err = $"Directory " + Path + " not found";
+                    buffer = Encoding.UTF8.GetBytes(err);
+                }
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+                Listening();
+            }
+        }
+        private byte[] getFile(string rawUrl, string main_page)
+        {
+            byte[] buffer = null;
+            var filePath = Path + rawUrl;
+
+            if (Directory.Exists(filePath))
+            {
+                filePath += main_page;
+                if (File.Exists(filePath))
+                    buffer = File.ReadAllBytes(filePath);
+                //Console.ReadKey();
+            }
+            else if (File.Exists(filePath))
+                buffer = File.ReadAllBytes(filePath);
+            return buffer;
+        }
+    }
+}
