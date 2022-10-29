ed8950ce3d08b30932a947dcbef2de10cd5dbf73
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-10-29 23:43:23 +0300

    Week5

--- Program.cs
+++ Program.cs
@@ -1,28 +1,73 @@
 ﻿using System;
 using INF2course.Week_4;
+using INF2course.Week_5;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace INF2course
 {
+    //WEEK4
+    //class ServerClass
+    //{
+    //    private bool running = true;
+    //    public bool Running { get => running; set { running = value; } }
+    //    public void Execute()
+    //    {
+    //        HttpServer server = new HttpServer(7700, @"./google", "google2.html");
+    //        server.Start();
+    //        while (running)
+    //            Handler(Console.ReadLine()?.ToLower(), server);
+    //    }
+    //    delegate void cmd(HttpServer server, ServerClass server_class);
+    //    static void Stop(HttpServer server, ServerClass server_class) { server.Stop(); }
+    //    static void Restart(HttpServer server, ServerClass server_class) { server.Start(); server.Stop(); }
+    //    static void Start(HttpServer server, ServerClass server_class) { server.Start(); }
+    //    static void Exit(HttpServer server, ServerClass server_class) { server_class.Running = false; }
+
+    //    Dictionary<string, cmd> dict = new Dictionary<string, cmd> { { "stop", Stop }, { "start", Start }, { "restart", Restart }, { "exit", Exit } };
+    //    void Handler(string command, HttpServer server)
+    //    {
+    //        if (dict.ContainsKey(command))
+    //            dict[command](server, this);
+    //    }
+    //}
+    //public class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        ServerClass server_class = new ServerClass();
+    //        server_class.Execute();
+    //    }
+    //}
+    
+    //Week5
     class ServerClass
     {
         private bool running = true;
         public bool Running { get => running; set { running = value; } }
         public void Execute()
         {
-            HttpServer server = new HttpServer(7700, @"./google", "google2.html");
+            StreamReader SR = new StreamReader("settings.json");
+            string open_json = SR.ReadLine();
+            SR.Close();
+            HttpServer2 server = JsonSerializer.Deserialize<HttpServer2>(open_json);
+
+
+            //HttpServer server = new HttpServer(7700, @"./google", "index.html");
+            server.Init();
             server.Start();
             while (running)
                 Handler(Console.ReadLine()?.ToLower(), server);
         }
-        delegate void cmd(HttpServer server, ServerClass server_class);
-        static void Stop(HttpServer server, ServerClass server_class) { server.Stop(); }
-        static void Restart(HttpServer server, ServerClass server_class) { server.Start(); server.Stop(); }
-        static void Start(HttpServer server, ServerClass server_class) { server.Start(); }
-        static void Exit(HttpServer server, ServerClass server_class) { server_class.Running = false; }
+        delegate void cmd(HttpServer2 server, ServerClass server_class);
+        static void Stop(HttpServer2 server, ServerClass server_class) { server.Stop(); }
+        static void Restart(HttpServer2 server, ServerClass server_class) { server.Stop(); server.Start(); }
+        static void Start(HttpServer2 server, ServerClass server_class) { server.Start(); }
+        static void Exit(HttpServer2 server, ServerClass server_class) { server_class.Running = false; }
 
         Dictionary<string, cmd> dict = new Dictionary<string, cmd> { { "stop", Stop }, { "start", Start }, { "restart", Restart }, { "exit", Exit } };
-        void Handler(string command, HttpServer server)
+        void Handler(string command, HttpServer2 server)
         {
             if (dict.ContainsKey(command))
                 dict[command](server, this);
---
+++ Week_5/HttpServer2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace INF2course.Week_5
+{
+    enum ServerStatus { Start, Stop };
+    internal class HttpServer2
+    {
+        public int Port { get; set; }
+        public string Path { get; set; }
+        public string MainPage { get; set; }
+        public ServerStatus Status = ServerStatus.Stop;
+        private readonly HttpListener http_listener;
+        public HttpServer2() { http_listener = new HttpListener(); }
+        public HttpServer2(int port, string path, string main_page)
+        {
+            Port = port; Path = path; MainPage = main_page;
+            http_listener = new HttpListener();
+            http_listener.Prefixes.Add($"http://localhost:" + Port + "/");
+        }
+        public void Init()
+        {
+            http_listener.Prefixes.Add($"http://localhost:" + Port + "/");
+        }
+        public void Start()
+        {
+            if (Status == ServerStatus.Start)
+            {
+                Console.WriteLine("Сервер уже запущен");
+                return;
+            }
+            Console.WriteLine("Запуск сервера...");
+            http_listener.Start();
+            Console.WriteLine("Сервер запущен");
+            Status = ServerStatus.Start;
+            Listening();
+        }
+        public void Stop()
+        {
+            if (Status == ServerStatus.Stop) return;
+            Status = ServerStatus.Stop;
+            Console.WriteLine("Остановка сервера...");
+            http_listener.Stop();
+            Console.WriteLine("Сервер остановлен");
+        }
+        private void Listening()
+        {
+            http_listener.BeginGetContext(new AsyncCallback(ListenerCallBack), http_listener);
+        }
+        private void ListenerCallBack(IAsyncResult result)
+        {
+            if (http_listener.IsListening && Status != ServerStatus.Stop)
+            {
+                var _httpContext = http_listener.EndGetContext(result);
+                HttpListenerRequest request = _httpContext.Request;
+                HttpListenerResponse response = _httpContext.Response;
+                byte[] buffer;
+                if (Directory.Exists(Path))
+                {
+                    buffer = getFile(_httpContext.Request.RawUrl, MainPage);
+                    if (buffer == null)
+                    {
+                        response.Headers.Set("Content-Type", "text/plain");
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        string err = "404 - not found";
+                        buffer = Encoding.UTF8.GetBytes(err);
+                    }
+                }
+                else
+                {
+                    string err = $"Directory " + Path + " not found";
+                    buffer = Encoding.UTF8.GetBytes(err);
+                }
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+                Listening();
+            }
+        }
+        private byte[] getFile(string rawUrl, string main_page)
+        {
+            byte[] buffer = null;
+            var filePath = Path + rawUrl;
+
+            if (Directory.Exists(filePath))
+            {
+                filePath += main_page;
+                if (File.Exists(filePath))
+                    buffer = File.ReadAllBytes(filePath);
+            }
+            else if (File.Exists(filePath))
+                buffer = File.ReadAllBytes(filePath);
+            return buffer;
+        }
+    }
+}
