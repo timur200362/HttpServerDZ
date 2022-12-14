acad13c778c6814c217a216d760342c46caf77d6
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-12-14 20:32:52 +0300

    commit5(test)

---
+++ Controllers/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF2course.Controllers
+{
+    internal class Session
+    {
+        public Guid Id { get; set; }
+        public int AccountId { get; set; }
+        public string Email { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
--- Controllers/SessionManager.cs
+++ Controllers/SessionManager.cs
@@ -8,5 +8,52 @@ namespace INF2course.Controllers
 {
     internal class SessionManager
     {
+        static Dictionary<Guid, Session> MemoryCache = new Dictionary<Guid, Session>();
+
+        public static Session Create(int accountId, string email)
+        {
+            var session = new Session
+            {
+                Id = Guid.NewGuid(),
+                AccountId = accountId,
+                Email = email,
+                CreateDateTime = DateTime.Now
+            };
+            MemoryCache.Add(session.Id, session);
+            return session;
+        }
+
+        public static bool IsValid(Guid sessionId)
+        {
+            if (!MemoryCache.ContainsKey(sessionId))
+            {
+                return false;
+            }
+            Session session = MemoryCache[sessionId];
+            bool result = session.CreateDateTime.AddMinutes(2) > DateTime.Now;
+            if (result == false)
+            {
+                MemoryCache.Remove(sessionId);
+            }
+
+            return result;
+        }
+
+        public static Session GetById(Guid sessionId)
+        {
+            if (!MemoryCache.ContainsKey(sessionId))
+            {
+                return null;
+            }
+            Session session = MemoryCache[sessionId];
+            bool result = session.CreateDateTime.AddMinutes(2) > DateTime.Now;
+            if (result == false)
+            {
+                MemoryCache.Remove(sessionId);
+                return null;
+            }
+
+            return session;
+        } 
     }
 }
